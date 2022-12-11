ecdfdc801ddaafa49e81b400105cb0ef7f4c98ab
droidsam1 <droidsam@gmail.com>
2022-12-11 13:14:27 +0100

    refactor: initialize productPrices in the constructor

--- Java/src/main/java/com/pduda/field/ShoppingCart.java
+++ Java/src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,12 +1,17 @@
 package com.pduda.field;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    private List<Integer> productPrices;
+    private final List<Integer> productPrices;
     private int price;
     private int numberOfProducts;
     private boolean hasDiscount;
+
+    public ShoppingCart() {
+        productPrices = new ArrayList<>();
+    }
 
     public void add(int price) {
         this.hasDiscount = price >= 100;
