4ffa65185ed8037522b1946e1c7ac60aef772582
droidsam1 <droidsam@gmail.com>
2022-12-11 13:12:45 +0100

    init migration steps: introducing a product prices list in the shopping cart

--- Java/src/main/java/com/pduda/field/ShoppingCart.java
+++ Java/src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,10 +1,11 @@
 package com.pduda.field;
 
+import java.util.List;
+
 public class ShoppingCart {
+    private List<Integer> productPrices;
     private int price;
-
     private int numberOfProducts;
-
     private boolean hasDiscount;
 
     public void add(int price) {
