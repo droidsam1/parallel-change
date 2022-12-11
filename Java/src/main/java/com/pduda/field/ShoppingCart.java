4942d87e679204e61f414e9596e090fdf35d19c1
droidsam1 <droidsam@gmail.com>
2022-12-11 12:38:41 +0100

    refactor: introduce a new shoppingCart property

--- Java/src/main/java/com/pduda/field/ShoppingCart.java
+++ Java/src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,7 +1,15 @@
 package com.pduda.field;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private final List<Integer> productPrices;
     private int price;
+
+    public ShoppingCart() {
+        productPrices = new ArrayList<>();
+    }
 
     public void add(int price) {
         this.price = price;
